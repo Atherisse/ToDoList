01f3373404930788f65c3dcc57055d0774808b0c
Apple Dev <Shinyatheris@gmail.com>
2022-06-03 07:15:39 +0200

    Initial Commit

---
+++ Shared/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  Shared
+//
+//  Created by user217621 on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
