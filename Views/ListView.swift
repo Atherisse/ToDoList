c3ec8466a49baaff28a25418a9b4c562b0485ab1
AppleDev <Shinyatheris@gmail.com>
2022-06-08 09:20:59 +0200

    [ToDoListApp] Adds a view for checkmarks
    This view is currently not functional, servers only as a prototype

---
+++ Views/ListView.swift
@@ -0,0 +1,40 @@
+//
+//  ListView.swift
+//  ToDoList (iOS)
+//
+//  Created by user217621 on 6/3/22.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    
+    @EnvironmentObject var listViewModel:ListViewModel
+    
+    var body: some View {
+        List {
+            ForEach(listViewModel.items) { item in
+                ListRowView(item: item)
+            }
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
+        }
+        .listStyle(PlainListStyle())
+        .navigationTitle("Todo List (no emoji)")
+        .navigationBarItems(
+            leading: EditButton(),
+            trailing:
+                NavigationLink("Add", destination: AddView())
+            )
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            ListView()
+        }
+        .environmentObject(ListViewModel())
+    }
+}
+
