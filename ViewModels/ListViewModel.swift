13fefeec33bb7db62e48a113da8598b93bca1fd7
AppleDev <Shinyatheris@gmail.com>
2022-06-09 09:20:15 +0200

    [ToDoListApp][Feature]Gets JSON encoded data from itemKey in UserDefaults

--- ViewModels/ListViewModel.swift
+++ ViewModels/ListViewModel.swift
@@ -11,6 +11,7 @@ import Foundation
 
 class ListViewModel: ObservableObject{
     @Published var items:[ItemModel] = []
+    let itemKey: String = "items_list"
 
     //when this class is instanced, the first thing it does is to call the getItems function
     init(){
@@ -18,12 +19,14 @@ class ListViewModel: ObservableObject{
     }
     
     func getItems(){ //this function adds 3 hard coded instances of ItemModel model, to our items array ln13
-        let newItems = [
-            ItemModel(title:"title", isCompleted: false),// instance of ItemModel with the two requried properties
-            ItemModel(title:"title2", isCompleted: true),
-            ItemModel(title:"title3", isCompleted: false)
-         ]
-        items.append(contentsOf: newItems)
+       
+        guard
+            let data = UserDefaults.standard.data(forKey: itemKey),
+            let savedItems = try? JSONDecoder().decode([ItemModel].self, from: data)
+        else { return }
+        
+        self.items = savedItems
+        
     }
     
     func deleteItem(indexSet: IndexSet){ // allows us to remove specific elements from the items array
